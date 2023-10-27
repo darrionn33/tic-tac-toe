8cabf3dc9b56e867c232e1c88bfad6ad3a599270
darrionn33 <darrionn33@gmail.com>
2023-10-27 19:14:22 +0530

    added first version of game logic

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
 const gameBoard = document.querySelector(".game-board");
-const scoreArray = ["X", "O", "X", "O", "X", "O", "X", "O", "O"];
+const scoreArray = ["", "", "", "", "", "", "", "", ""];
+
+const endGame = (winner) => {
+  scoreArray.forEach((item, index) => {
+    scoreArray[index] = "";
+    console.log(winner + " wins!");
+  });
+  const buttons = document.querySelectorAll("button");
+  buttons.forEach((button) => {
+    button.textContent = "";
+  });
+};
+const checkWin = () => {
+  const winConditionsArray = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+  ];
+  winConditionsArray.forEach((winCondition) => {
+    if (
+      scoreArray[winCondition[0]] === "X" &&
+      scoreArray[winCondition[1]] === "X" &&
+      scoreArray[winCondition[2]] === "X"
+    ) {
+      endGame("X");
+    } else if (
+      scoreArray[winCondition[0]] === "O" &&
+      scoreArray[winCondition[1]] === "O" &&
+      scoreArray[winCondition[2]] === "O"
+    ) {
+      endGame("O");
+    } else {
+      // console.log("Draw!");
+    }
+  });
+};
 
 scoreArray.forEach((score, index) => {
   const button = document.createElement("button");
@@ -10,5 +50,6 @@ scoreArray.forEach((score, index) => {
   button.onclick = () => {
     button.textContent = button.textContent === "X" ? "O" : "X";
     scoreArray[index] = button.textContent;
+    checkWin();
   };
 });
