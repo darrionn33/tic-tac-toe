f2f558cf9375a12606632d054578104a51a3401a
darrionn33 <darrionn33@gmail.com>
2023-10-28 18:01:44 +0530

    added a start menu

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const gameBoard = document.querySelector(".game-board");
-const scoreArray = ["", "", "", "", "", "", "", "", ""];
-let currentTurn = "X";
-let currentMove = 0;
 
-const endGame = (winner) => {
+const endGame = (winner, scoreArray) => {
   scoreArray.forEach((item, index) => {
     scoreArray[index] = "";
   });
@@ -17,14 +14,19 @@ const endGame = (winner) => {
   } else {
     winnerDiv.textContent = winner + " won!";
   }
+  const restartButton = document.createElement("button");
+  restartButton.textContent = "RESTART";
   winnerDiv.classList.add("winner");
+  winnerDiv.appendChild(restartButton);
   gameBoard.appendChild(winnerDiv);
-  setTimeout(() => {
+  restartButton.onclick = () => {
+    console.log("reset");
     gameBoard.removeChild(winnerDiv);
-  }, 1000);
+  };
   currentMove = 0;
+  console.log("currentMove:", currentMove);
 };
-const checkWin = () => {
+const checkWin = (scoreArray, currentMove, reset) => {
   const winConditionsArray = [
     [0, 1, 2],
     [3, 4, 5],
@@ -35,50 +37,95 @@ const checkWin = () => {
     [1, 4, 7],
     [2, 5, 8],
   ];
-  winConditionsArray.forEach((winCondition) => {
+  winConditionsArray.every((winCondition) => {
     if (
       scoreArray[winCondition[0]] === "X" &&
       scoreArray[winCondition[1]] === "X" &&
       scoreArray[winCondition[2]] === "X"
     ) {
-      endGame("X");
+      reset();
+      endGame("X", scoreArray);
     } else if (
       scoreArray[winCondition[0]] === "O" &&
       scoreArray[winCondition[1]] === "O" &&
       scoreArray[winCondition[2]] === "O"
     ) {
-      endGame("O");
+      reset();
+      endGame("O", scoreArray);
     } else if (currentMove === 9) {
-      endGame("Draw");
+      reset();
+      endGame("Draw", scoreArray);
+    }
+    if (currentMove === 9) {
+      return false;
+    } else {
+      return true;
     }
   });
 };
 
-scoreArray.forEach((score, index) => {
-  const button = document.createElement("button");
-  button.textContent = score;
-  button.setAttribute("data-index", index);
-  gameBoard.appendChild(button);
-
-  button.onclick = () => {
-    if (scoreArray[index] === "") {
-      switch (currentTurn) {
-        case "X":
-          button.textContent = "X";
-          currentTurn = "O";
-          document.querySelector(".x").classList.toggle("active");
-          document.querySelector(".o").classList.toggle("active");
-          break;
-        case "O":
-          button.textContent = "O";
-          currentTurn = "X";
-          document.querySelector(".x").classList.toggle("active");
-          document.querySelector(".o").classList.toggle("active");
-          break;
-      }
-      scoreArray[index] = button.textContent;
-      currentMove = currentMove + 1;
-      checkWin();
-    }
+const startGame = (players) => {
+  gameBoard.removeChild(document.querySelector(".menu"));
+  const scoreArray = ["", "", "", "", "", "", "", "", ""];
+  let currentTurn = "X";
+  let currentMove = 0;
+  const resetCurrentMove = () => {
+    currentMove = 0;
+    console.log(currentMove);
   };
-});
+  switch (players) {
+    case 1:
+      break;
+    case 2:
+      scoreArray.forEach((score, index) => {
+        const button = document.createElement("button");
+        button.textContent = score;
+        button.setAttribute("data-index", index);
+        gameBoard.appendChild(button);
+
+        button.onclick = () => {
+          if (scoreArray[index] === "") {
+            switch (currentTurn) {
+              case "X":
+                button.textContent = "X";
+                currentTurn = "O";
+                document.querySelector(".x").classList.toggle("active");
+                document.querySelector(".o").classList.toggle("active");
+                break;
+              case "O":
+                button.textContent = "O";
+                currentTurn = "X";
+                document.querySelector(".x").classList.toggle("active");
+                document.querySelector(".o").classList.toggle("active");
+                break;
+            }
+            scoreArray[index] = button.textContent;
+            currentMove = currentMove + 1;
+            console.log("currentMove:", currentMove);
+            checkWin(scoreArray, currentMove, resetCurrentMove);
+          }
+        };
+      });
+      break;
+  }
+};
+
+document.querySelector("#start-button").onclick = () => {
+  document.querySelector(".menu").replaceChildren();
+  const btn1 = document.createElement("button");
+  btn1.id = "1P";
+  btn1.textContent = "1 Player";
+  btn1.setAttribute("disabled", "true");
+  const btn2 = document.createElement("button");
+  btn2.id = "2P";
+  btn2.textContent = "2 Players";
+  document.querySelector(".menu").appendChild(btn1);
+  document.querySelector(".menu").appendChild(btn2);
+
+  btn1.onclick = () => {
+    startGame(1);
+  };
+  btn2.onclick = () => {
+    startGame(2);
+  };
+};
